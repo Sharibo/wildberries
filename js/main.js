8945ddc6a379f132e64629fc1b0c543944e3e524
Sharibo <SystemofCybermirrorsWorld@gmail.com>
2021-03-26 20:09:10 +0300

    2 day: navigation

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
-//* cart
+//* modal cart
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
 const modalClose = document.querySelector('.modal-close');
@@ -47,9 +47,8 @@ modalCart.addEventListener('click', function (event) {
 }
 
 //* goods
-
 const more = document.querySelector('.more');
-const navigationItem = document.querySelectorAll('.navigation-item');
+const navigationLink = document.querySelectorAll('.navigation-link');
 const longGoodsList = document.querySelector('.long-goods-list');
 
 const getGoods = async function () {
@@ -61,17 +60,17 @@ const getGoods = async function () {
 	return await result.json();
 }
 
-const createCard = function (objCard) {
+const createCard = function ({ label, img, name, description, id, price }) {
 	const card = document.createElement('div');
 	card.className = 'col-lg-3 col-sm-6'
 	card.innerHTML = `
 	<div class="goods-card">
-		${objCard.label ? `<span class="label">${objCard.label}</span>` : ''}		
-		<img src="db/${objCard.img}" alt="${objCard.name}" class="goods-image">
-		<h3 class="goods-title">${objCard.name}</h3>
-		<p class="goods-description">${objCard.description}</p>
-		<button class="button goods-card-btn add-to-cart" data-id="${objCard.id}">
-			<span class="button-price">$${objCard.price}</span>
+		${label ? `<span class="label">${label}</span>` : ''}		
+		<img src="db/${img}" alt="${name}" class="goods-image">
+		<h3 class="goods-title">${name}</h3>
+		<p class="goods-description">${description}</p>
+		<button class="button goods-card-btn add-to-cart" data-id="${id}">
+			<span class="button-price">$${price}</span>
 		</button>
 	</div>
 	`;
@@ -90,3 +89,28 @@ more.addEventListener('click', function (event) {
 	event.preventDefault();
 	getGoods().then(renderCards);
 });
+
+//* filtered goods
+const filterCards = function (field, value) {
+	getGoods().then(function (data) {
+		const filteredGoods = data.filter(function (good) {
+			return good[field] === value
+		});
+		return filteredGoods
+
+	}).then(renderCards);
+}
+
+navigationLink.forEach(function (link) {
+	link.addEventListener('click', function (event) {
+		event.preventDefault();
+		const field = link.dataset.field;
+		const value = link.textContent;
+		if (field === undefined) {
+			getGoods().then(renderCards);
+		} else {
+			filterCards(field, value);
+		}
+	});
+});
+
