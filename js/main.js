f928a2645ff615e554959861d8a0ff6ee13b9d29
Sharibo <SystemofCybermirrorsWorld@gmail.com>
2021-03-26 18:47:54 +0300

    2 day: goods

--- js/main.js
+++ js/main.js
@@ -13,17 +13,17 @@ const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
 const modalClose = document.querySelector('.modal-close');
 
-const openModal = function() {
+const openModal = function () {
 	modalCart.classList.add('show')
 };
 
-const closeModal = function() {
-	modalCart.classList.remove('show')
-};
+const closeModal = function () {
+	modalCart.classList.remove('show');
+}
 
 buttonCart.addEventListener('click', openModal);
 modalClose.addEventListener('click', closeModal);
-modalCart.addEventListener('click', function(event) {
+modalCart.addEventListener('click', function (event) {
 	if (event.target.classList.contains('overlay')) {
 		closeModal();
 	}
@@ -34,14 +34,59 @@ modalCart.addEventListener('click', function(event) {
 {
 	const scrollLinks = document.querySelectorAll('a.scroll-link');
 
-	for (let i = 0; i < scrollLinks.length; i++) {
-		scrollLinks[i].addEventListener('click', function (event) {
+	for (const scrollLink of scrollLinks) {
+		scrollLink.addEventListener('click', function (event) {
 			event.preventDefault();
-			const id = scrollLinks[i].getAttribute('href');
+			const id = scrollLink.getAttribute('href');
 			document.querySelector(id).scrollIntoView({
 				behavior: 'smooth',
 				block: 'start',
-			})
+			});
 		});
 	}
 }
+
+//* goods
+
+const more = document.querySelector('.more');
+const navigationItem = document.querySelectorAll('.navigation-item');
+const longGoodsList = document.querySelector('.long-goods-list');
+
+const getGoods = async function () {
+	const result = await fetch('db/db.json');
+	if (!result.ok) {
+		throw 'Ошибочка вышла: ' + result.status
+	}
+
+	return await result.json();
+}
+
+const createCard = function (objCard) {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6'
+	card.innerHTML = `
+	<div class="goods-card">
+		${objCard.label ? `<span class="label">${objCard.label}</span>` : ''}		
+		<img src="db/${objCard.img}" alt="${objCard.name}" class="goods-image">
+		<h3 class="goods-title">${objCard.name}</h3>
+		<p class="goods-description">${objCard.description}</p>
+		<button class="button goods-card-btn add-to-cart" data-id="${objCard.id}">
+			<span class="button-price">$${objCard.price}</span>
+		</button>
+	</div>
+	`;
+
+	return card;
+}
+
+const renderCards = function (data) {
+	longGoodsList.textContent = '';
+	const cards = data.map(createCard);
+	longGoodsList.append(...cards);
+	document.body.classList.add('show-goods');
+}
+
+more.addEventListener('click', function (event) {
+	event.preventDefault();
+	getGoods().then(renderCards);
+});
